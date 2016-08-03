4abca9e41d8e03914a3e742e7916edf23aadd461
Gianluca Esposito <g@esposi.to>
2016-08-03 12:23:36 +0200

    Add ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "extends": "airbnb",
+    "installedESLint": true,
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "indent": 0,
+        "key-spacing": 0,
+        "quote-props": 0,
+        "no-trailing-spaces": [2, { "skipBlankLines": true }],
+
+        "react/jsx-filename-extension": 0,
+        "react/jsx-indent-props": 0,
+        "react/prefer-es6-class": 0,
+        "react/no-string-refs": 0,
+    }
+};
